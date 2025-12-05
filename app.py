03d7d2afa7f9d8767fbebcde67651492a7a6062b
yaalasan <rahmaniyazovy@gmail.com>
2025-12-05 20:29:48 +0800

    Fix app huggingface

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
